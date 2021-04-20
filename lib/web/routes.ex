be2cb855e5cdd963f165e73c46ba15c50da13048
Mayel <git@mayel.space>
2021-04-20 15:36:28 +0200

    add routes

---
+++ lib/web/routes.ex
@@ -0,0 +1,40 @@
+defmodule Bonfire.Recyclapp.Routes do
+  defmacro __using__(_) do
+
+    quote do
+
+      alias Bonfire.Recyclapp.Routes.Helpers, as: SocialRoutes
+
+      pipeline :recyclapp do
+        plug :put_root_layout, {Bonfire.Recyclapp.LayoutView, :root}
+      end
+
+      # pages anyone can view
+      scope "/bread", Bonfire.Recyclapp do
+        pipe_through :browser
+
+      end
+
+      # pages you need an account to view
+      scope "/bread", Bonfire.Recyclapp do
+        pipe_through :browser
+        pipe_through :account_required
+
+      end
+
+      # VF pages you need to view as a user
+      scope "/recyclapp", Bonfire.Recyclapp do
+        pipe_through :browser
+        pipe_through :user_required
+        pipe_through :recyclapp
+
+        live "/", RecyclappDashboardLive
+        live "/settings", RecyclappSettingsLive
+        live "/success/:reciprocal_id", RecyclappSuccessLive
+
+        live "/map/", MapLive
+        live "/map/:id", MapLive
+      end
+    end
+  end
+end
