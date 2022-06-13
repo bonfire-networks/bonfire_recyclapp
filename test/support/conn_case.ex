cef750dcaf200b7122f7141054d1695bfbd0db80
Mayel de Borniol <git@mayel.space>
2022-06-14 09:01:07 +1200

    Use compile_env

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -33,7 +33,7 @@ defmodule Bonfire.Recyclapp.ConnCase do
       # alias Bonfire.Recyclapp.Web.Router.Helpers, as: Routes
 
       # The default endpoint for testing
-      @endpoint Bonfire.Common.Config.get!(:endpoint_module)
+      @endpoint Application.compile_env!(:bonfire, :endpoint_module)
     end
   end
 
