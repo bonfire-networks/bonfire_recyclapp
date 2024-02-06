b6c2951cd5f983804cfce55fea14cfa16f9f16d4
Mayel de Borniol <git@mayel.space>
2024-02-06 18:53:37 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/243

--- priv/repo/migrations/20201105094943_import_me.exs
+++
@@ -1,9 +0,0 @@
-defmodule Bonfire.Recyclapp.Repo.Migrations.ImportMe do
-  @moduledoc false
-  use Ecto.Migration
-
-  import Bonfire.Recyclapp.Migration
-  # accounts & users
-
-  def change, do: migrate_me
-end
