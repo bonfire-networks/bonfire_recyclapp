435646328e5a0ed9077009e70d34371a7e2867f2
Mayel de Borniol <git@mayel.space>
2025-05-03 20:07:34 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1239

--- lib/integration.ex
+++ lib/integration.ex
@@ -1,5 +1,6 @@
 defmodule Bonfire.Recyclapp do
   @moduledoc "./README.md" |> File.stream!() |> Enum.drop(1) |> Enum.join()
+  use Bonfire.Common.Config
 
   def repo, do: Bonfire.Common.Config.repo()
 
